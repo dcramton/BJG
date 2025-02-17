863f6f2d14b0c866545513556db4878309b9f186
Derek Cramton <dcramton@yahoo.com>
2025-02-17 10:08:02 -0500

    02172025a

---
+++ static/aws-exports.js
@@ -0,0 +1,10 @@
+const awsconfig = {
+    Auth: {
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_AhOZ6qpQh',
+        userPoolWebClientId: '7iafa06ln6h47pv38r164jrldl',
+        identityPoolId: 'us-east-1:fdb064dd-9a8f-42bc-b7e3-0a50d88633b1'
+    }
+};
+
+export default awsconfig;
